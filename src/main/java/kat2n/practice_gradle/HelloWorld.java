60e150182c2ea8f71311368f11f4a532e38f6ba9
kat2n <katsumata.coffee.150yen@gmail.com>
2019-06-18 22:50:50 -0700

    add new files

---
+++ src/main/java/kat2n/practice_gradle/HelloWorld.java
@@ -0,0 +1,15 @@
+package kat2n.practice_gradle;
+
+import java.io.PrintStream;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    print(System.out);
+  }
+
+  public static void print(PrintStream out) {
+    out.println("Hello Wrold!");
+  }
+
+}
---
+++ src/test/java/MyTest.java
@@ -0,0 +1,17 @@
+import kat2n.practice_gradle.HelloWorld;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class MyTest {
+  @Test
+  public void name() throws Exception {
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    HelloWorld.print(new PrintStream(out));
+    String s = out.toString();
+    Assert.assertEquals("Hello", s);
+  }
+}
